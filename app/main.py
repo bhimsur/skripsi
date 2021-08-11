27d2be65ea007f4d195498ec5b026cf55ab9b21e
bhimantoro <bhimantoros@gmail.com>
2021-08-12 01:54:20 +0700

    SVM and RF predict endpoint

--- app/main.py
+++ app/main.py
@@ -2,21 +2,37 @@ from starlette.responses import RedirectResponse
 from typing import List
 from pydantic import BaseModel
 from fastapi import FastAPI
-import keras
+# import keras
 import joblib
 import sklearn
 
 
 app = FastAPI()
 
-cnn_model = keras.models.load_model('model/cnn_final.h5')
+# cnn_model = keras.models.load_model('model/cnn_final.h5')
 tfidf_model = joblib.load('model/tfidf_final.pkl')
 rf_model = joblib.load('model/rf_final.pkl')
 svm_model = joblib.load('model/svm_final.pkl')
 
 class Text(BaseModel):
+  method: str
   text: str
 
 @app.get('/')
-async def index():
+def index():
   return RedirectResponse(url='/docs')
+
+@app.post('/predict')
+def predict(data: Text):
+  tfidf_transform = tfidf_model.transform([data.text])
+  if data.method == 'rf':
+    res = rf_model.predict_proba(tfidf_transform)[0]
+  elif data.method == 'svm':
+    res = svm_model.predict_proba(tfidf_transform)[0]
+  predict = dict({"negatif":res[0]*100, "positif":res[1]*100})
+  return {
+    "status":True,
+    "message":"success",
+    "probability":predict,
+    "prediction":"positif" if predict["positif"] > predict["negatif"] else "negatif"
+  } 
