6e2cca19e8232e3f85423709cf03fdb350764f2e
bhimantoro <bhimantoros@gmail.com>
2021-08-12 02:44:15 +0700

    add endpoint for neural network

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ from starlette.responses import RedirectResponse
 from typing import List
 from pydantic import BaseModel
 from fastapi import FastAPI
-# import keras
+import keras
 import joblib
 import sklearn
+from keras.preprocessing.sequence import pad_sequences
 
 
 app = FastAPI()
 
-# cnn_model = keras.models.load_model('model/cnn_final.h5')
+cnn_model = keras.models.load_model('model/cnn_final.h5')
 tfidf_model = joblib.load('model/tfidf_final.pkl')
 rf_model = joblib.load('model/rf_final.pkl')
 svm_model = joblib.load('model/svm_final.pkl')
@@ -30,10 +31,35 @@ def predict(data: Text):
     res = rf_model.predict_proba(tfidf_transform)[0]
   elif data.method == 'svm':
     res = svm_model.predict_proba(tfidf_transform)[0]
-  predict = dict({"negatif":res[0]*100, "positif":res[1]*100})
+  else:
+    return {"status":False, "message":"method not allowed"}
   return {
     "status":True,
     "message":"success",
-    "probability":predict,
-    "prediction":"positif" if predict["positif"] > predict["negatif"] else "negatif"
-  } 
+    "probability":{"negatif":res[0], "positif":res[1]},
+    "prediction":"positif" if res[1] > res[0] else "negatif"
+  }
+
+@app.post('/predict/nn')
+def predict_neural_network(data:Text):
+  data_sequences = tokenizer_model.texts_to_sequences([data.text])
+  data_sequences = pad_sequences(data_sequences, maxlen=52)
+  if data.method == 'cnn':
+    res = cnn_model.predict([data_sequences])
+    res = res.tolist()[0][0]
+  else:
+    return {'status':False, 'message':'method not allowed'}
+
+  if res <= 0.5:
+    negatif = res
+    positif = 1-res
+  else:
+    negatif = 1-res
+    positif = res
+    
+  return {
+    'status':True,
+    'message':'success',
+    'probability': {'negatif':negatif, 'positif':positif},
+    'prediction':'positif' if positif > negatif else 'negatif'
+  }
